088cc1f1bf7bad54e5f7e3c8776e22fc691ecacb
AbishekKhanal <abishekkhanal2056@gmail.com>
2025-11-06 12:34:22 +0545

    Add authentication and admin role requirement to user listing endpoint

--- src/components/users/router/user.router.js
+++ src/components/users/router/user.router.js
@@ -1,9 +1,11 @@
 import express from "express";
 
 import { listAllUsers } from "../controllers/list.user.controller.js";
+import { authenticateToken } from "../../../middleware/auth.middleware.js";
+import { requireAdmin } from "../../../middleware/role.middleware.js";
 
 const user = express.Router();
 
-user.get("/", listAllUsers);
+user.get("/", authenticateToken, requireAdmin, listAllUsers);
 
 export default user;
